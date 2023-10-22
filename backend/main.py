b034c0f5a24c35680f076bc851c420bac229f5e2
un4rch <unaielorriaga07@gmail.com>
2023-10-22 13:37:31 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,40 @@
+# Libreria para configurar el servidor
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Union
+import importlib
+
+routes = {
+    "/": "/routes/HelloWorld.py",
+    "/pregunta": "/routes/Helloworld.py"
+}
 
 class Item(BaseModel):
     name: str
 
 app = FastAPI()
+for route in routes:
+    @app.get(route)
+    async def root():
+        return {"message": routes[route]}
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+    @app.post(route)
+    async def hacerPregunta(item: Item):
+        # Importa el módulo usando importlib
+        hello_module = importlib.import_module(routes[route][:-3])  # Elimina la extensión '.py'
 
-@app.post("/pregunta/")
-async def hacerPregunta(item: Item):
-    return item
+        # Crea una instancia de la clase Hello
+        hello_instance = hello_module.Hello()
+        """ esto en Hello.py
+        # Hello.py
+        class Hello:
+            def say_hello(self):
+                print("Hola desde la clase Hello!")
+        """
+
+        # Llama a un método de la clase
+        hello_instance.doPost(item)
+        return hello_instance.doPost(item)
 
 """ Peticion HTTP con BurpSuite
 POST /pregunta/ HTTP/1.1
