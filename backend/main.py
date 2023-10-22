6595c3a7f66b6a1c7dc68892433fcab1ad7037cb
un4rch <unaielorriaga07@gmail.com>
2023-10-22 18:31:52 +0200

    uvicorn config

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Union
+import uvicorn
+from dotenv import load_dotenv
+import os
 
 class Item(BaseModel):
     name: str
@@ -14,6 +17,15 @@ async def root():
 @app.post("/pregunta/")
 async def hacerPregunta(item: Item):
     return item
+
+if __name__ == "__main__":
+    # Cargar el fichero .env en variables de entorno del sistema
+    load_dotenv()
+    # Recuperar la variable del sistema que nos interesa (socket de confguracion para el backend)
+    socket = os.environ.get("BACKEND_SOCKET")
+    [host, port] = socket.split(":")
+    # Iniciar el servidor
+    uvicorn.run(app, host=host, port=int(port))
 
 """ Peticion HTTP con BurpSuite
 POST /pregunta/ HTTP/1.1
