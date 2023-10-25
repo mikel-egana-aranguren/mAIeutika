6a19182001d8e47c455fe82b8bf298f99b0bffde
un4rch <unaielorriaga07@gmail.com>
2023-10-25 16:44:40 +0200

    http requests

--- frontend/src/app/components/chat-content/chat-content.component.ts
+++ frontend/src/app/components/chat-content/chat-content.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ChatService } from 'src/app/services/chat.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-chat-content',
@@ -9,11 +10,12 @@ import { ChatService } from 'src/app/services/chat.service';
 })
 export class ChatContentComponent {
   private subscription: Subscription = new Subscription();
-  messages: {"role": string, "content": string}[] = [
+  /*messages: {"role": string, "content": string}[] = [
     {"role": "system", "content": "Eres un asistente muy útil."},
     {"role": "user", "content": "Que dia es"},
     {"role": "assistant", "content": "23 de octubre"}
-  ];
+  ];*/
+  messages: {"role": string, "content": string}[] = []
 
   /*constructor(private chatService: ChatService) {
     this.subscription = this.chatService.message$.subscribe(
@@ -25,10 +27,11 @@ export class ChatContentComponent {
       }
     );
   }*/
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private httpClient: HttpClient) {
     this.subscription = this.chatService.message$.subscribe(
       (message: string) => {
         this.messages.push({"role": "user", "content": message});
+        this.sendPostRequest(message);
       }
     );
   }
@@ -36,6 +39,25 @@ export class ChatContentComponent {
   ngOnDestroy() {
     // Importante: Desuscribirse para evitar pérdida de memoria
     this.subscription.unsubscribe();
+  }
+
+  private sendPostRequest(message: string): void {
+    const apiUrl = 'http://127.0.0.1:8000/pregunta/';
+    const requestBody = { "role": 'user', "prompt": message };
+
+    this.httpClient.post(apiUrl, requestBody).subscribe(
+      (response) => {
+        console.log('POST request successful:', typeof(response));
+        if (typeof(response) === "string") {
+          this.messages.push({"role": "assistant", "content": response});
+        }
+        // Handle the response if needed
+      },
+      (error) => {
+        console.error('Error during POST request:', error);
+        // Handle errors if needed
+      }
+    );
   }
 
 }
