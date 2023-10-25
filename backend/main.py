0d9c0796c9539fd3b0f5531d05942b77ecd7e57f
un4rch <unaielorriaga07@gmail.com>
2023-10-25 16:43:35 +0200

    server update

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Union
+import uvicorn
+from dotenv import load_dotenv
+import os
+import chatGPT
+import json
 
 class Item(BaseModel):
-    name: str
+    prompt: str
 
 app = FastAPI()
+chat = chatGPT.Chat()
+
+# Allow all origins during development, replace "*" with your frontend's URL in production
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -13,7 +29,16 @@ async def root():
 
 @app.post("/pregunta/")
 async def hacerPregunta(item: Item):
-    return item
+    return chat.makePrompt(item.prompt)
+
+if __name__ == "__main__":
+    # Cargar el fichero .env en variables de entorno del sistema
+    load_dotenv()
+    # Recuperar la variable del sistema que nos interesa (socket de confguracion para el backend)
+    socket = os.environ.get("BACKEND_SOCKET")
+    [host, port] = socket.split(":")
+    # Iniciar el servidor
+    uvicorn.run(app, host=host, port=int(port))
 
 """ Peticion HTTP con BurpSuite
 POST /pregunta/ HTTP/1.1
