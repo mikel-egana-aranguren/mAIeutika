86bb252347d6a89a963633ddd1d155db7bb65731
un4rch <unaielorriaga07@gmail.com>
2023-10-25 16:44:57 +0200

    temporary init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-conda activate ChatTFG
-pip install "fastapi[all]" openai librosa rich typer "uvicorn[standard]" 2&>/dev/null
-cd backend
-uvicorn main:app --reload
+#conda activate ChatTFG
+#pip install "fastapi[all]" openai librosa rich typer "uvicorn[standard]" 2&>/dev/null
+#cd backend
+#uvicorn main:app --reload
+sudo docker compose up -d
